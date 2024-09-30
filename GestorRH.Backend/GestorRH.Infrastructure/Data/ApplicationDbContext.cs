4991e4269076065199bf59bee6047b812d70eb0e
Rogério Piveto <127915397+pivetoo@users.noreply.github.com>
2024-09-30 20:12:48 -0300

    implementação do dbcontext

---
+++ GestorRH.Backend/GestorRH.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using GestorRH.Dominio.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorRH.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Cargo> Cargos { get; set; }
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Ferias> Ferias { get; set; }
+        public DbSet<BatidaPonto> BatidasPonto { get; set; }
+        public DbSet<Relatorio> Relatorios { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Funcionario>()
+                .HasOne(f => f.Cargo)
+                .WithMany()
+                .HasForeignKey(f => f.CargoId);
+
+            modelBuilder.Entity<BatidaPonto>()
+                .HasOne(bp => bp.Funcionario)
+                .WithMany()
+                .HasForeignKey(bp => bp.FuncionarioId);
+        }
+    }
+}
